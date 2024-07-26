7365cc2b1bbb9dfa58f9491d8232e727911403d0
simo blues <wambuisimon712@gmail.com>
2024-07-26 21:52:59 +0300

    changes

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -1,7 +1,42 @@
+import { useContext } from "react"
 import "./navbar.scss"
+import { DarkModeContext } from "../../context/darkModeContext"
+// import { AuthContext } from "../../context/authContext"
+import { Link } from "react-router-dom"
+import { IoHomeOutline, IoPersonOutline, IoSearchOutline, IoSunnyOutline } from "react-icons/io5"
+import {  MdOutlineDarkMode, MdOutlineEmail, MdOutlineGridView } from "react-icons/md"
+import { BiNotification } from "react-icons/bi"
+
 const Navbar = () => {
+  const { toggle, darkMode} = useContext(DarkModeContext)
+  // const { currentUser} = useContext(AuthContext)
+
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration: "none"}}>
+        <span>blueHike</span>
+        </Link>
+        <IoHomeOutline />
+        <div className="darkmode">
+        {darkMode ? (<IoSunnyOutline onClick={toggle} />) : (<MdOutlineDarkMode onClick={toggle} />)}
+        </div>
+        <MdOutlineGridView />
+        <div className="search">
+          <IoSearchOutline />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+      <div className="right">
+        <IoPersonOutline/>
+        <MdOutlineEmail/>
+        <BiNotification/>
+        {/* <div className="user">
+          <img src={currentUser.profilePic} alt="" />
+          <span>{currentUser.name}</span>
+        </div> */}
+      </div>
+    </div>
   )
 }
 
---
+++ src/componets/post/Post.jsx
@@ -0,0 +1,8 @@
+import "./post.scss"
+const Post = () => {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
